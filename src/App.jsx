02a70f2be33bfa70944e1f5e16ff2b7e1896628c
raMitov <chizmywoe@gmail.com>
2024-12-15 22:35:29 +0200

    zdrave da e

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import Navigation from './components/Navigation/Navigation.jsx'; 
 import Logo from './components/Logo/Logo';  
 import Rank from './components/Rank/Rank';
-
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm.jsx';  
 import ParticlesComponent from './components/Particles/Particles.jsx';
-
-
+import { Component } from 'react';
 import './App.css'
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value); 
+  }
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+  render(){
   return (
     <>
       <ParticlesComponent id="particles" />  
       <Navigation />
         <Logo />
        <Rank />
-       <ImageLinkForm />
+       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
        {/*
         <FaceRecognition /> */}
     </>
   )
+}
 }
 
 export default App
